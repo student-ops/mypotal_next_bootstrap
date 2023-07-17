b235e35a7cf24cbec1c8e986a4d0a2ee8ccd8bdd
stundet-ops <gogakuec@gmail.com>
2023-07-17 18:46:07 +0900

    add iframe test

--- my-project/src/pages/index.tsx
+++ my-project/src/pages/index.tsx
@@ -4,21 +4,22 @@ import { Dropdown } from 'react-bootstrap';
 export default function Home() {
   return (
     <>
-      <p>Hello Bootstrap</p>
+      <div>
+        <p>Hello Bootstrap</p>
+        <Dropdown>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            Dropdown
+          </Dropdown.Toggle>
 
-      <Dropdown>
-        <Dropdown.Toggle variant="primary" id="dropdown-basic">
-          Dropdown
-        </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Action 3</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <h1>Hello</h1>
+      </div>
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Action 3</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-
-      <h1>Hello</h1>
     </>
   )
 }
