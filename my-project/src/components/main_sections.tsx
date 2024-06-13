ab218457fa46780af66ee277bcebd75a2d24b8da
student-ops <student-ops@example.com>
2024-06-13 13:53:52 +0900

    add moon to works and refactor styling

--- my-project/src/components/main_sections.tsx
+++ my-project/src/components/main_sections.tsx
@@ -11,6 +11,7 @@ import {
   faBook,
   faCoffee,
 } from "@fortawesome/free-solid-svg-icons";
+import { link } from "fs";
 
 const introduce_header = "h6 fw-bold";
 const Section1 = (
@@ -61,6 +62,19 @@ const Section1 = (
 //   </div>
 // );
 
+const linkStyle =
+  "text-primary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover";
+
+const githubLink = (link: string) => {
+  return (
+    <a
+      className="text-primary link-offset-2 link-offset-3-hover link-underline-opacity-0 link-underline-opacity-75-hover text-decoration-underline"
+      href={link}
+    >
+      GitHub リポジトリ
+    </a>
+  );
+};
 const Section3 = (
   <div>
     <h1 className="fw-bold fs-2 mb-3">Works</h1>
@@ -68,67 +82,82 @@ const Section3 = (
     {/* <Col xs="auto"> */}
     <Container>
       <p className="pb-3">
-        個人開発で作成したポートフォリオです。ソースコードも同じ名前でGitHubに公開しています。
+        これまでに個人やハッカソンで作成したアプリケーションやソフトウェアです。
       </p>
       {/* <div className="inner py-3" style={{ borderTop: "solid 1px #d1d1d1" }}> */}
+
       <div className="">
-        <div className="title_github d-flex justify-content-between ">
-          <a
-            href="https://app.croud-crab.com"
-            target="_blank"
-            className="image"
-          >
+        <div className="inner py-3">
+          <div className="title_github d-flex justify-content-between">
+            <a href="https://moon-usa.jp/" target="_blank" className="image">
+              <div style={{ display: "inline-block", paddingRight: "0.3rem" }}>
+                <h4 className="">Moon</h4>
+              </div>
+              <span>
+                <i className="fas fa-external-link-alt"></i>
+              </span>
+            </a>
+          </div>
+          <p>
+            Moonはハッカソンをきっかけに作成したLINE
+            Botで、「青春を取りこぼさない」をテーマに、遊びの予定を思いついてから実行に移すプロセスの中で出てくる様々なめんどくさいタスクを全てAIにやらせてしまおう！という考えのもと開発されたスケジューリングサービスです。
+            <br />
+            <br />
+            開発のきっかけとなった
+            <a
+              href="https://tornado-official.jp/tornado2023/"
+              className={linkStyle}
+            >
+              Tornadoハッカソン
+            </a>
+            では最優秀賞を受賞し、さらにその後も開発を継続し
+            <a
+              href="https://lineapiusecase.com/ja/community/linedc-award-2024.html"
+              className={linkStyle}
+            >
+              LINE DC BOT AWARDS 2024
+            </a>
+            でも奨励賞を受賞しています。
+          </p>
+        </div>
+        <div className="inner py-3" style={{ borderTop: "solid 1px #d1d1d1" }}>
+          <div className="title_github d-flex justify-content-between ">
             <div style={{ display: "inline-block", paddingRight: "3px" }}>
               <h4>Efforsition</h4>
             </div>
-            <span>
-              <i className="fas fa-external-link-alt "></i>
-            </span>
-          </a>
-          <a
-            className="github_link text-info text-decoration-underline"
-            href="https://github.com/student-ops/efforsiton"
-          >
-            GitHub リポジトリ
-          </a>
-        </div>
-        <a href="https://app.croud-crab.com">
-          {/* <Image
+            {githubLink("https://github.com/student-ops/efforsiton")}
+          </div>
+          {/* <a href="https://app.croud-crab.com">
+          <Image
             src="/effosition_app.png"
             alt="Effortion"
             width="100%"
             className="rounded mt-2 mb-3"
-          /> */}
-        </a>
+          />
+        </a> */}
 
-        <p className="pb-4">
-          <span className="fw-bold">
-            EfforsitionはGitHub、chatgpt
-            apiと密に連動した開発者向けのタスク管理アプリです。
-          </span>
-          <br />
-          cahtgpt
-          apiを使用してコードの差分から実装された機能を推測し、開発の進捗状況を把握することが目的です。
-          <br />
-          ユーザーはGithubアカウントでサインインし、Webhookを使用してレポジトリの更新を検知します。
-          <br />
-          検知した更新をもとにアプリ内で作成したタスクの達成を自動で判定、管理します。
-        </p>
+          <p className="">
+            <span className="fw-bold">
+              EfforsitionはGitHub、chatgpt
+              apiと密に連動した開発者向けのタスク管理アプリです。
+            </span>
+            <br />
+            cahtgpt
+            apiを使用してコードの差分から実装された機能を推測し、開発の進捗状況を把握することが目的です。
+            <br />
+            ユーザーはGithubアカウントでサインインし、Webhookを使用してレポジトリの更新を検知します。
+            <br />
+            検知した更新をもとにアプリ内で作成したタスクの達成を自動で判定、管理します。
+          </p>
+        </div>
       </div>
-      <a href="#" className="image"></a>
+
       <div className="inner py-3" style={{ borderTop: "solid 1px #d1d1d1" }}>
-        {/* <div className="inner "> */}
         <div className="title_github d-flex justify-content-between">
           <a href="#" target="_blank" className="image">
             <h4>Goチャットサービス</h4>
-            {/* <i className="fas fa-external-link-alt"></i> */}
           </a>
-          <a
-            className="github_link text-info text-decoration-underline"
-            href="https://github.com/student-ops/recuruit"
-          >
-            GitHub リポジトリ
-          </a>
+          {githubLink("https://github.com/student-ops/recuruit")}
         </div>
         <p>
           初めて作ったWebアプリです。一般的な掲示板でGoとPostgresのみで実装しています。
@@ -142,12 +171,7 @@ const Section3 = (
             <h4>Line Openai bot</h4>
             {/* <i className="fas fa-external-link-alt"></i> */}
           </a>
-          <a
-            className="github_link text-info text-decoration-underline"
-            href="https://github.com/student-ops/line_api_test"
-          >
-            GitHub リポジトリ
-          </a>
+          {githubLink("https://github.com/student-ops/line_api_test")}
         </div>
         <p>
           Openai Nodesdkを使用して実装しました。LineトークからOpenAIの
