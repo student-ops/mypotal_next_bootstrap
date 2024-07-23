814bd051b2c80b3e48240e01676acc79272070fb
student-ops <student-ops@example.com>
2024-07-23 22:32:48 +0900

    update iframe height and adjust screenview

--- src/pages/bot.tsx
+++ src/pages/bot.tsx
@@ -1,9 +1,19 @@
-import exp from "constants";
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+
 const BotPage: React.FC = () => {
+  const instRef = useRef<HTMLParagraphElement>(null);
+  const [iframeHeight, setIframeHeight] = useState("100%");
+
+  useEffect(() => {
+    if (instRef.current) {
+      const instHeight = instRef.current.offsetHeight;
+      setIframeHeight(`calc(100% - ${instHeight} - 40px)`);
+    }
+  }, []);
+
   return (
-    <div className="h-full w-full">
-      <p>
+    <div className="vh-100 d-flex flex-column">
+      <p ref={instRef} className="inst">
         パンダマスターです どんな質問でもパンダに当てはめて回答してくれます
         <br />
         (例) <br />
@@ -13,9 +23,20 @@ const BotPage: React.FC = () => {
       </p>
       <iframe
         src="https://udify.app/chatbot/XVkYwnWl8X6T2nBp"
-        style={{ width: "90%", height: "95%", minHeight: "700px" }}
+        className="flex-grow-1"
+        style={{
+          width: "100%",
+          height: iframeHeight,
+          minHeight: "100px",
+        }}
         allow="microphone"
       ></iframe>
+      <div
+        className=""
+        style={{
+          height: "40px",
+        }}
+      ></div>
     </div>
   );
 };
